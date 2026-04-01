1085cc6397a6dd89724f74c78befe8d585ee1e70
zojaP <zojaperfileva@gmail.com>
2026-04-01 08:53:36 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// id="select-location
+const selectLocation = document.querySelector("#select-location");
+
+selectLocation.addEventListener("click", function () {
+    const selectList = selectLocation.querySelector(".select__list");
+    selectList.classList.toggle("select__list--visible");
+});
+
+var listItems = selectLocation.querySelectorAll(".select__list-item");
+listItems.forEach(function (listItem) {
+    listItem.addEventListener("click", function (event) {
+        // event.stopPropagation();
+        selectLocation
+            .querySelector(".select__list-item--active")
+            .classList.remove("select__list-item--active");
+        this.classList.add("select__list-item--active");
+        const selectedlistItem = selectLocation.querySelector(".select__value");
+        selectedlistItem.innerText = this.innerText;
+    });
+});
+
+// id = "select-property";
+const selectProperty = document.querySelector("#select-property");
+selectProperty.addEventListener("click", function () {
+    const selectList = selectProperty.querySelector(".select__list");
+    selectList.classList.toggle("select__list--visible");
+});
+var listItems = selectProperty.querySelectorAll(".select__list-item");
+listItems.forEach(function (listItem) {
+    listItem.addEventListener("click", function (event) {
+        // event.stopPropagation();
+        selectProperty
+            .querySelector(".select__list-item--active")
+            .classList.remove("select__list-item--active");
+        this.classList.add("select__list-item--active");
+        const selectedlistItem = selectProperty.querySelector(".select__value");
+        selectedlistItem.innerText = this.innerText;
+    });
+} );
+
+// id="select-price"
+const selectPrice = document.querySelector("#select-price");
+
+selectPrice.addEventListener("click", function () {
+    const selectList = selectPrice.querySelector(".select__list");
+    selectList.classList.toggle("select__list--visible");
+});
+
+var listItems = selectPrice.querySelectorAll(".select__list-item");
+listItems.forEach(function (listItem) {
+    listItem.addEventListener("click", function (event) {
+        // event.stopPropagation();
+        selectPrice
+            .querySelector(".select__list-item--active")
+            .classList.remove("select__list-item--active");
+        this.classList.add("select__list-item--active");
+        const selectedlistItem = selectPrice.querySelector(".select__value");
+        selectedlistItem.innerText = this.innerText;
+    });
+});
+
+
+const buttonApril = document.querySelector(".thanksWebCademy");
+buttonApril.addEventListener( "click", function () {
+    document.querySelector(".header__img").classList.add("none");
+    document.querySelector(".foto-april1").classList.remove("none");
+})
